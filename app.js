aa06d092116f9142560c3786913429309c5e1a9b
Gustavo Miño <gus.desarrollo.web@gmail.com>
2025-09-22 07:23:58 -0300

    Validacion del formulario con Js, campos de contraseña

--- app.js
+++ app.js
@@ -1,1 +1,20 @@
-console.log("Hoal")
+const d = document;
+
+const $formulario = d.getElementById("form");
+const $inputPassword = d.getElementById("password");
+const $inputPasswordConfirm = d.getElementById("password-confirm");
+
+const submit = (e) => {
+  e.preventDefault();
+
+  if ($inputPassword.value === $inputPasswordConfirm.value) {
+    $inputPassword.style.borderColor = "596d48";
+    $inputPasswordConfirm.style.borderColor = "#596d48";
+    $formulario.submit();
+  } else {
+    $inputPassword.style.borderColor = "#e09b9b";
+    $inputPasswordConfirm.style.borderColor = "#e09b9b";
+  }
+};
+
+$formulario.addEventListener("submit", submit);
